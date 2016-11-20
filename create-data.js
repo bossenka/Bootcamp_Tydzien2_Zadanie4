22543322e8dfed43ce7883d1163c590ef8b9c94b
bossenka <fatyga.m.90@gmail.com>
2016-11-20 19:07:43 +0100

    Add solution.

---
+++ create-data.js
@@ -0,0 +1,22 @@
+function createData(obj) {
+
+    var data = obj;
+
+    return {
+      get: function(key) {
+        return data[key];
+      },
+      set: function(key, value) {
+        if (key && value) {
+          data[key] = value;
+        }
+      }
+    };
+
+}
+
+var data = createData({});
+
+data.set("name", "Janek");
+
+console.log( data.get("name") );
